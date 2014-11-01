dfdfbcccf6fd2e7dd107357de1010fddd8973ea7
Itamar Tzadok <itamar@substantialmethods.com>
2014-11-01 15:58:09 -0400

    Interim version update.

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
  */
 
 $plugin->component = 'mod_dataform';
-$plugin->release  = '2.7';
-$plugin->version = 2014072100;
+$plugin->release  = '2.8 Beta';
+$plugin->version = 2014110100;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
 // $plugin->cron     = 60;
